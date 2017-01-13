6dc742deea17119762c9e3749f269ae19c885d25
hectorcoronado <coronado.hector@gmail.com>
2017-01-13 11:20:35 -0800

    search-bar controlled comp, binding onInputChange to this

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  // Initialize component state:
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+
+    /*
+    We need to bind the correct 'this' environment to our onInputChange function. We're declaring
+    the context of 'this' as the SearchBar component:
+    */
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        {/* Make this a controlled component with a value & onChange's function */}
+        <input
+          placeholder="5-day city forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
